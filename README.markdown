9f0989059a0aaabcaa85ebddc6d1b09d87b19a71
maclovin <eric@dev-2.local>
2012-07-24 17:14:45 -0300

    Agora funfa no Linux

--- README.markdown
+++ README.markdown
@@ -2,16 +2,16 @@ naodeutempo
 ===========
 
 ##O que é?
-
 Tá ligado aquelas ofertas relampagos que surgem em sites de compras coletivas? Tipo aquela viagem para Buenos Aires por R$2,00 que você perdeu a 1 minuto atrás? Não perca mais!
 
 
 ##Só funciona no Groupon!
-
 `ruby naodeutempo.rb [endereco_da_oferta] "[frase_de_alerta]"`
 
 ##Exemplo:
-
 `ruby naodeutempo.rb http://www.groupon.com.br/ofertas/oferta-nacional "Yo dude"`
 
 Ao ativar a oferta o script te avisa.
+
+##Requisitos
+Unix, Ruby, e as bibliotecas nokogiri e open-uri
--- naodeutempo.rb
+++ naodeutempo.rb
@@ -4,6 +4,36 @@ require "bundler/setup"
 require 'nokogiri'
 require 'open-uri'
 
+def windows?
+  (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+end
+
+def mac?
+ (/darwin/ =~ RUBY_PLATFORM) != nil
+end
+
+def unix?
+  !windows?
+end
+
+def linux?
+  unix? and not mac?
+end
+
+
+def say(text)
+  puts text
+
+  if mac?
+    `say #{text}`
+  elsif unix? and linux?
+    `echo "#{text}"|espeak`
+  else
+    raise 'OS not supported'
+  end
+end
+
+
 url =  ARGV[0]
 speak = ARGV[1]
 
@@ -13,7 +43,10 @@ while(true)
   offer = Nokogiri::HTML(open(url))
 
   status = offer.css('.boxBlue .priceTag div').text
-  `say #{speak}` if(status != 'Oferta esgotada')
+
+  if status != 'Oferta esgotada'
+    say(speak)
+  end
 
   sleep 10
 end
